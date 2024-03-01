e9c63aa36896d973fb36305237624ddda9d7a02d
JustynaAnna <justynkapawlik@gmail.com>
2024-03-01 12:45:36 +0100

    Implemented password validation feature
    Added functionality to validate password entered by users.

--- script.js
+++ script.js
@@ -81,25 +81,18 @@ const clearForm = () => {
     el.value = "";
     clearError(el);
   });
+  requirementList.classList.remove("display-list");
 };
 
 sendBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  checkForm([username, pass, pass2, email]);
+  checkForm([username, pass2, email]);
   checkLength(username, 3);
-  checkLength(pass, 8);
   checkPassword(pass, pass2);
   checkMail(email);
   checkErrors();
 });
 
-// clearBtn.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   const arrForm = Array.from(form);
-//   arrForm.forEach((input) => {
-//     input.value = "";
-//   });
-// });
 clearBtn.addEventListener("click", (event) => {
   event.preventDefault();
   clearForm();
@@ -108,4 +101,42 @@ clearBtn.addEventListener("click", (event) => {
 closePopup.addEventListener("click", () => {
   popup.classList.remove("show-popup");
   clearForm();
+});
+//TUTAJ TESTUJE KOD
+
+const eyeIcon = document.querySelector(".fa-eye");
+const requirementList = document.querySelector(".requirement-list");
+const list = document.querySelectorAll(".requirement-list li");
+
+// An array of password requirements with corresponding
+// regular expressions and index of the requirement list item
+const requirements = [
+  { regex: /.{8,}/, index: 0 }, // Minimum of 8 characters
+  { regex: /[0-9]/, index: 1 }, // At least one number
+  { regex: /[a-z]/, index: 2 }, // At least one lowercase letter
+  { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
+  { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
+];
+
+pass.addEventListener("keyup", (e) => {
+  requirementList.classList.add("display-list");
+  requirements.forEach((item) => {
+    // Check if the password matches the requirement regex
+    const isValid = item.regex.test(e.target.value);
+    const requirementItem = list[item.index];
+
+    // Updating class and icon of requirement item if requirement matched or not
+    if (isValid) {
+      requirementItem.classList.add("valid");
+      requirementItem.firstElementChild.className = "fa-solid fa-check";
+    } else {
+      requirementItem.classList.remove("valid");
+      requirementItem.firstElementChild.className = "fa-solid fa-circle";
+    }
+  });
+});
+
+eyeIcon.addEventListener("click", () => {
+  // Toggle the password input type between "password" and "text"
+  pass.type = pass.type === "password" ? "text" : "password";
 });
