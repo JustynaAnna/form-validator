dda69d9470cb7819f709deabf180ed4279063490
JustynaAnna <justynkapawlik@gmail.com>
2024-03-01 17:11:28 +0100

    Implement password validation and form submission logic.
    Refactor variable and function names

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const username = document.querySelector("#username");
-const pass = document.querySelector("#password");
-const pass2 = document.querySelector("#password2");
-const email = document.querySelector("#email");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const pass2Input = document.querySelector("#password2");
+const emailInput = document.querySelector("#email");
 const sendBtn = document.querySelector(".send");
 const clearBtn = document.querySelector(".clear");
-const popup = document.querySelector(".popup");
-const closePopup = document.querySelector(".close");
+const popupModal = document.querySelector(".popup");
+const closeModalBtn = document.querySelector(".close");
 // const form = document.querySelector("form");
 
 const showError = (input, msg) => {
@@ -21,46 +21,40 @@ const clearError = (input) => {
   formBox.classList.remove("error");
 };
 
-const checkForm = (input) => {
+const validateNonEmptyFields = (input) => {
   input.forEach((el) => {
     if (el.value === "") {
-      showError(el, el.placeholder);
+      showError(el, "This field is required");
     } else {
       clearError(el);
     }
   });
 };
 
-const checkLength = (input, min) => {
+const validateFieldLength = (input, min) => {
   if (input.value.length < min) {
-    showError(
-      input,
-      `${input.previousElementSibling.innerText.slice(
-        0,
-        -1
-      )} should be at least ${min} letters long`
-    );
+    showError(input, `This field must be at least ${min} characters long`);
   }
 };
 
-const checkPassword = (pass1, pass2) => {
+const validatePasswordMatch = (pass1, pass2) => {
   if (pass1.value !== pass2.value) {
-    showError(pass2, "The passwords don't match");
+    showError(pass2, "Passwords do not match");
   }
 };
 
-const checkMail = (email) => {
+const validateEmail = (email) => {
   const mailRegex =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   if (mailRegex.test(email.value)) {
     clearError(email);
   } else {
-    showError(email, "The email is invalid");
+    showError(email, "Invalid email format");
   }
 };
 
-//Symulacja wysyłania formularza
+//The function checkErrors checks for errors in the form and simulates submission
 
 const checkErrors = () => {
   const allInputs = document.querySelectorAll(".form-box");
@@ -72,25 +66,37 @@ const checkErrors = () => {
     }
   });
   if (errorCount === 0) {
-    popup.classList.add("show-popup");
+    popupModal.classList.add("show-popup");
   }
 };
 
 const clearForm = () => {
-  [username, pass, pass2, email].forEach((el) => {
+  [usernameInput, passwordInput, pass2Input, emailInput].forEach((el) => {
     el.value = "";
     clearError(el);
   });
-  requirementList.classList.remove("display-list");
+  requirementListItem.classList.remove("display-list");
+};
+
+// Funkcja obsługująca wysłanie formularza
+const sendForm = () => {
+  validateNonEmptyFields([usernameInput, pass2Input, emailInput]);
+  validateFieldLength(usernameInput, 3);
+  validatePasswordMatch(passwordInput, pass2Input);
+  validateEmail(emailInput);
+  checkErrors();
 };
 
 sendBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  checkForm([username, pass2, email]);
-  checkLength(username, 3);
-  checkPassword(pass, pass2);
-  checkMail(email);
-  checkErrors();
+  sendForm();
+});
+
+usernameInput.addEventListener("keypress", (e) => {
+  if (e.key === "Eneter") {
+    e.preventDefault();
+    sendForm();
+  }
 });
 
 clearBtn.addEventListener("click", (event) => {
@@ -98,35 +104,40 @@ clearBtn.addEventListener("click", (event) => {
   clearForm();
 });
 
-closePopup.addEventListener("click", () => {
-  popup.classList.remove("show-popup");
+closeModalBtn.addEventListener("click", () => {
+  popupModal.classList.remove("show-popup");
   clearForm();
 });
 //TUTAJ TESTUJE KOD
 
-const eyeIcon = document.querySelector(".fa-eye");
-const requirementList = document.querySelector(".requirement-list");
+const passwordVisibilityIcon = document.querySelector(".fa-eye");
+const requirementListItem = document.querySelector(".requirement-list");
 const list = document.querySelectorAll(".requirement-list li");
 
 // An array of password requirements with corresponding
 // regular expressions and index of the requirement list item
-const requirements = [
-  { regex: /.{8,}/, index: 0 }, // Minimum of 8 characters
-  { regex: /[0-9]/, index: 1 }, // At least one number
-  { regex: /[a-z]/, index: 2 }, // At least one lowercase letter
-  { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
-  { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
-];
 
-pass.addEventListener("keyup", (e) => {
-  requirementList.classList.add("display-list");
-  requirements.forEach((item) => {
-    // Check if the password matches the requirement regex
-    const isValid = item.regex.test(e.target.value);
-    const requirementItem = list[item.index];
+const checkPasswordRequirements = (password) => {
+  const requirements = [
+    { label: "Minimum of 8 characters", regex: /.{8,}/ },
+    { label: "At least one number", regex: /[0-9]/ },
+    { label: "At least one lowercase letter", regex: /[a-z]/ },
+    { label: "At least one special character", regex: /[^A-Za-z0-9]/ },
+    { label: "At least one uppercase letter", regex: /[A-Z]/ },
+  ];
 
-    // Updating class and icon of requirement item if requirement matched or not
-    if (isValid) {
+  return requirements.map((requirement) => ({
+    label: requirement.label,
+    isValid: requirement.regex.test(password),
+  }));
+};
+
+//Funkcja, która daje ptaszki do listy
+const updatePasswordRequirements = (value) => {
+  requirementListItem.classList.add("display-list");
+  checkPasswordRequirements(value).forEach((requirement, index) => {
+    const requirementItem = list[index];
+    if (requirement.isValid) {
       requirementItem.classList.add("valid");
       requirementItem.firstElementChild.className = "fa-solid fa-check";
     } else {
@@ -134,9 +145,16 @@ pass.addEventListener("keyup", (e) => {
       requirementItem.firstElementChild.className = "fa-solid fa-circle";
     }
   });
+};
+
+passwordInput.addEventListener("keyup", (e) => {
+  updatePasswordRequirements(e.target.value);
 });
 
-eyeIcon.addEventListener("click", () => {
+const togglePasswordVisibility = () => {
   // Toggle the password input type between "password" and "text"
-  pass.type = pass.type === "password" ? "text" : "password";
+  passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+};
+passwordVisibilityIcon.addEventListener("click", () => {
+  togglePasswordVisibility();
 });
